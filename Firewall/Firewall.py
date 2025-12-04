ef775b6bcebf8f2f0f84046448971772c7f06cf8
Izzy_K <G00424523@atu.ie>
2025-12-04 17:01:40 +0000

    Implement parse_packet() function which extracts packet metadata for IP, TCP, UDP, ICMP & "other" - protocol, source/dest IPs & ports - & returns info in readable string format

--- Firewall/Firewall.py
+++ Firewall/Firewall.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from scapy.all import IP, TCP, UDP, ICMP
 
 # Create loggers
 logging.basicConfig(level=logging.INFO)
@@ -19,3 +20,28 @@ def log_allowed(info):
 
 def log_denied(info):
     deny_logger.info(f"{datetime.now()} DROP {info}")
+
+def parse_packet(pkt):
+    # Extract readable metadata from the packet.
+    scapy_pkt = IP(pkt.get_payload())
+
+    src = scapy_pkt.src
+    dst = scapy_pkt.dst
+
+    # Detect protocol + ports
+    if scapy_pkt.haslayer(TCP):
+        protocol = "TCP"
+        sport = scapy_pkt[TCP].sport
+        dport = scapy_pkt[TCP].dport
+    elif scapy_pkt.haslayer(UDP):
+        protocol = "UDP"
+        sport = scapy_pkt[UDP].sport
+        dport = scapy_pkt[UDP].dport
+    elif scapy_pkt.haslayer(ICMP):
+        protocol = "ICMP"
+        sport = dport = "-"
+    else:
+        protocol = f"OTHER({scapy_pkt.proto})"
+        sport = dport = "-"
+
+    return f"{protocol} {src}:{sport} -> {dst}:{dport}"
