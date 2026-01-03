a8f797343a19c2d2f7030bb15048d84995abe9fa
Izzy_K <G00424523@atu.ie>
2026-01-03 18:55:02 +0000

    Update firewall functionality - add stateful connection logic to auto-allow packets from established connections

--- Firewall/Firewall.py
+++ Firewall/Firewall.py
@@ -17,11 +17,72 @@ deny_handler = logging.FileHandler("denied.log")
 allow_logger.addHandler(allow_handler)
 deny_logger.addHandler(deny_handler)
 
+# Create table to track established connections - allows stateful connection tracking
+connection_table = set()
+
 def log_allowed(info):
     allow_logger.info(f"{datetime.now()} ALLOW {info}")
 
 def log_denied(info):
     deny_logger.info(f"{datetime.now()} DROP {info}")
+
+# helper method for extracting connection information from a packet
+def get_flow_id(pkt):
+    # create variables to store packet's protocol and ports
+    protocol = None
+    sport = dport = None
+
+    # Extract packet's protocol - check if TCP or UDP
+    if pkt.haslayer(TCP):
+        protocol = "TCP"
+        sport = pkt[TCP].sport
+        dport = pkt[TCP].dport
+    elif pkt.haslayer(UDP):
+        protocol = "UDP"
+        sport = pkt[UDP].sport
+        dport = pkt[UDP].dport
+    else:
+        return None  # only track TCP/UDP for now
+
+    # return a tuple that uniquely identifies a flow - so future packets can be identified as part of an established flow (connection) or part of a new flow 
+    return (
+        pkt.src, pkt.dst,
+        sport, dport,
+        protocol
+    )
+
+# method that tracks connections by storing flows in a table (set)
+def track_connection(pkt):
+    # use helper method to extract packet's flow characteristics  
+    flow = get_flow_id(pkt)
+    
+    if not flow:
+        return
+
+    # extract flow details from flow tuple
+    src, dst, sport, dport, protocol = flow
+
+    # Store both directions of the connection because connections are bidirectional & incoming/outgoing packets have different src/dst addresses and ports - this way packets going in either direction can be matched to an existing connection
+
+    # egress direction
+    connection_table.add((src, dst, sport, dport, protocol, "OUTBOUND"))
+
+    # ingress direction
+    connection_table.add((dst, src, dport, sport, protocol, "INBOUND"))
+
+# helper method to check if a packet is part of an established connection - i.e. in the connection table
+def is_established(pkt):
+    # extract flow information from packet using helper method
+    flow = get_flow_id(pkt)
+    if not flow:
+        return False
+
+    # extract flow details from flow tuple
+    src, dst, sport, dport, protocol = flow
+    # check if flow exists in connection table in either direction & return result (True/False)
+    return ( (src, dst, sport, dport, protocol, "INBOUND") in connection_table or
+    (src, dst, sport, dport, protocol, "OUTBOUND") in connection_table )
+
 
 def parse_packet(pkt):
     # Extract readable metadata from the packet.
@@ -52,29 +113,37 @@ def process_packet(pkt):
      # Parse packet and log initial info
     scapy_pkt = IP(pkt.get_payload())
     info = parse_packet(pkt)
-    log_allowed(info)
 
     # Allow Rules -----------------------------
     
+    # allow established connections
+    if is_established(scapy_pkt):
+        log_allowed(f"{info} [ESTABLISHED]")
+        pkt.accept()
+        return
+    
     # Allow all ICMP traffic
     if scapy_pkt.haslayer(ICMP):
-        log_allowed(info)
+        # ICMP traffic is not tracked as it is stateless
+        log_allowed(f"{info} [NEW]")
         pkt.accept()
         return
 
     # Allow HTTP and HTTPS traffic
     if scapy_pkt.haslayer(TCP) and scapy_pkt[TCP].dport in (80, 443):
-        log_allowed(info)
+        log_allowed(f"{info} [NEW]")
+        track_connection(scapy_pkt)
         pkt.accept()
         return
     
     # Allow DNS queries
     if scapy_pkt.haslayer(UDP) and scapy_pkt[UDP].dport == 53:
-        log_allowed(info)
+        log_allowed(f"{info} [NEW]")
+        track_connection(scapy_pkt)
         pkt.accept()
         return
     
-    # Deny Rules ------------------------------
+    # Deny all other traffic & log
     log_denied(info)
     pkt.drop()
 
